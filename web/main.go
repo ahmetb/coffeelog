bf2afdd73aa04164be87c18fd9c0817203ef1de1
Ahmet Alp Balkan <ahmetb@google.com>
2017-02-15 09:44:51 -0800

    web: save cookie securely after login
    Signed-off-by: Ahmet Alp Balkan <ahmetb@google.com>

--- web/main.go
+++ web/main.go
@@ -8,14 +8,22 @@ import (
 	"os"
 
 	plus "github.com/google/google-api-go-client/plus/v1"
+	"github.com/gorilla/securecookie"
 	"github.com/pkg/errors"
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
 )
 
-var cfg *oauth2.Config
+var (
+	cfg      *oauth2.Config
+	hashKey  = []byte("very-secret")      // TODO extract to env
+	blockKey = []byte("a-lot-secret-key") // TODO extract to env
+	sc       = securecookie.New(hashKey, blockKey)
+)
 
 func main() {
+	sc.SetSerializer(securecookie.JSONEncoder{})
+
 	// read oauth2 config
 	env := "GOOGLE_OAUTH2_CONFIG"
 	if os.Getenv(env) == "" {
@@ -56,35 +64,59 @@ func login(w http.ResponseWriter, r *http.Request) {
 
 func oauth2Callback(w http.ResponseWriter, r *http.Request) {
 	if state := r.URL.Query().Get("state"); state != "todo_rand_state" {
-		w.WriteHeader(http.StatusBadRequest)
-		fmt.Fprint(w, "wrong state")
+		badRequest(w, errors.New("wrong oauth2 state"))
+		return
 	}
 
 	code := r.URL.Query().Get("code")
 	if code == "" {
-		w.WriteHeader(http.StatusBadRequest)
-		fmt.Fprint(w, "missing oauth2 grant code")
+		badRequest(w, errors.New("missing oauth2 grant code"))
 		return
 	}
 
 	tok, err := cfg.Exchange(oauth2.NoContext, code)
 	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		fmt.Fprint(w, errors.Wrap(err, "oauth2 token exchange failed"))
+		badRequest(w, errors.Wrap(err, "oauth2 token exchange failed"))
 		return
 	}
-	svc, err := plus.New(cfg.Client(oauth2.NoContext, tok))
+
+	tokenSource := cfg.TokenSource(oauth2.NoContext, tok)
+	svc, err := plus.New(oauth2.NewClient(oauth2.NoContext, tokenSource))
 	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		fmt.Fprint(w, errors.Wrap(err, "failed to construct g+ client"))
+		badRequest(w, errors.Wrap(err, "failed to construct g+ client"))
 		return
 	}
 	me, err := plus.NewPeopleService(svc).Get("me").Do()
 	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		fmt.Fprint(w, errors.Wrap(err, "failed to query user g+ profile"))
+		badRequest(w, errors.Wrap(err, "failed to query user g+ profile"))
 		return
 	}
+
+	// encrypt the cookie
+	newToken, err := tokenSource.Token()
+	if err != nil {
+		badRequest(w, errors.Wrap(err, "failed to extract token from tokensource"))
+		return
+	}
+	tokEncoded, err := sc.Encode("oauth2_token", newToken)
+	if err != nil {
+		badRequest(w, errors.Wrap(err, "failed to encode the token"))
+		return
+	}
+
+	// TODO save cookies
+	http.SetCookie(w, &http.Cookie{
+		Name:  "oauth2_token",
+		Path:  "/",
+		Value: tokEncoded,
+	})
+
 	log.Printf("Logged in user id: %s", me.Id)
-	fmt.Fprintf(w, "Hello "+me.DisplayName)
+	fmt.Fprintf(w, "Hello "+me.DisplayName+" ("+fmt.Sprintf("%v", me.IsPlusUser)+")")
+	// TODO redirect to somewhere meaningful
+}
+
+func badRequest(w http.ResponseWriter, err error) {
+	w.WriteHeader(http.StatusBadRequest)
+	fmt.Fprint(w, errors.Wrap(err, "bad request"))
 }
